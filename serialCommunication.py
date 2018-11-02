d67d616d1850eb03eb18e5e044fb7ddeb07bc8ba
Aborren <elias.renman00@gmail.com>
2018-11-02 11:36:23 +0100

    fixed minor issues aswell as tweaked reading accuracy

--- serialCommunication.py
+++ serialCommunication.py
@@ -2,10 +2,7 @@ import time
 import datetime
 import serial
 
-
-from dBConnetion import open_connection,close_connection
-
-
+from dBConnetion import open_connection, close_connection
 
 
 def loop():
@@ -18,36 +15,46 @@ def loop():
 
 def add_to_database(arr):
     my_db = open_connection()
-    
+
     my_cursor = my_db.cursor()
 
-    sql = "INSERT INTO outside (date_outside, time_outside, temp_outside) VALUES (%s,%s,%s)"
+    sql = "INSERT INTO outside (date, time, temp) VALUES (%s,%s,%s)"
     my_cursor.execute(sql, arr)
     my_db.commit()
     print("sql sent : " + sql)
     close_connection(my_db)
 
 
-def get_request(serial):
-    print ("serial reading....")
+def get_reading(serial):
+    # print ("serial reading....")
 
     request = serial.readline()
-    print ("serial reading is complete.")
+    # print ("serial reading is complete.")
     request = request.rstrip("\n")
-    arr = [time.strftime("%Y-%m-%d"),time.strftime("%H:%M"),request]
-    print (time.strftime("%Y-%m-%d")+" "+time.strftime("%H:%M"))
+
+    return request
+
+
+def request_reading(serial):
+    request = 0
+    for x in range(5):
+
+        serial.write('1')
+        temp_reading = get_reading(serial)
+        if x == 0:
+            request = temp_reading
+        else:
+            request = (float(request) + float(temp_reading)) / 2
+        time.sleep(1)
+
+    arr = [time.strftime("%Y-%m-%d"), time.strftime("%H:%M"), request]
     add_to_database(arr)
+    print arr
     print ("#########################newline#######################")
 
 
-def send_request(serial):
-
-    serial.write('1')
-    get_request(serial)
-
-
 def serial_io():
-    send_request(ser)
+    request_reading(ser)
 
 
 ser = serial.Serial(
